003112d74ce574b0f1f258f1984108aaa4f9c948
Sanghyuk Jeong <shjeong920522@gmail.com>
2021-04-21 15:00:01 +0900

    add create-room api

--- music_controller/api/serializers.py
+++ music_controller/api/serializers.py
@@ -1,12 +1,15 @@
 from rest_framework import serializers
-from . import models
+from .models import Room
+
 
 class RoomSerializer(serializers.ModelSerializer):
     class Meta:
-        model = models.Room
-        fields = ('id', 'code', 'host', 'guest_can_pause', 'votes_to_skip', 'created_at')
-        
+        model = Room
+        fields = ('id', 'code', 'host', 'guest_can_pause',
+                  'votes_to_skip', 'created_at')
+
+
 class CreateRoomSerializer(serializers.ModelSerializer):
     class Meta:
-        model = models.Room
+        model = Room
         fields = ('guest_can_pause', 'votes_to_skip')
--- music_controller/api/urls.py
+++ music_controller/api/urls.py
@@ -1,7 +1,8 @@
+
 from django.urls import path
-from . import views
+from .views import RoomView, CreateRoomView
 
 urlpatterns = [
-    path('room', views.RoomView.as_view()),
-    path('create', views.CreateRoomView.as_view()),
+    path('room', RoomView.as_view()),
+    path('create-room', CreateRoomView.as_view())
 ]
